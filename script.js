5afb6b797e0dd8a56b97827bb57c26fcb7535a5e
martomorri <106319796+martomorri@users.noreply.github.com>
2023-03-29 09:05:42 -0300

    29-3

--- script.js
+++ script.js
@@ -2,31 +2,54 @@ let toDoList = [
     {
         tarea: "Hacer tarea de matematica",
         realizado: false,
-        fechaCreacion: Date.now(),
-        fechaRealizado: new Date
+        fechaCreacion: new Date,
+        fechaRealizado: null
     },
     {
         tarea: "Leer Judia",
         realizado: false,
-        fechaCreacion: Date.now(),
-        fechaRealizado: new Date
+        fechaCreacion: new Date,
+        fechaRealizado: null
     }
 ];
 
+let ul = document.getElementById("toDoList");
+const mostrarList = () => {
+    ul.innerHTML = toDoList.map(toDo => `<li><label><input id="check" type="checkbox"> ${toDo.tarea}</label></li>`).join("");
+}
+mostrarList();
+
+let checkBox = document.getElementById("check");
+checkBox.onclick = (e) => {
+    if (checkBox.checked) {
+        e.target.style.textDecoration = "line-through";
+        console.log("tachado");
+    }
+}
+
 let input = document.getElementById("tarea");
 let button = document.getElementById("enviar");
-//button.onclick = agregarTarea(input.value);
 
-const agregarTarea = nuevaTarea => {    
-    toDoList.push({
-        tarea: nuevaTarea,
-        realizado: false,
-        fechaCreacion: Date.now(),
-        fechaRealizado: new Date
-    })
+button.onclick = () => {    
+    if (input.value.length == 0) {
+        input.style.border = "1px solid red";
+        alert("Complete los datos");
+    }
+    else {
+        ul.innerHTML = "";
+        toDoList.push({
+            tarea: input.value,
+            realizado: false,
+            fechaCreacion: Date.now(),
+            fechaRealizado: new Date
+        });
+        /*let li = document.createElement("li");
+        li.innerHTML = `<label><input class="check" type="checkbox"> ${input.value}</label>`;
+        ul.appendChild(li);*/
+        mostrarList();
+        input.style.border = "1px solid black";
+        input.value = "";
+    }
 };
-
-let div = document.getElementById("toDoList");
-div.innerHTML = toDoList.map(toDo => `<label><input type="checkbox"> ${toDo.tarea}</label><br>`).join("");
 
 console.log(toDoList);
