9f7011479b3146ac07310bec0e1a31aa13496e55
martomorri <106319796+martomorri@users.noreply.github.com>
2023-03-30 10:39:42 -0300

    terminado, falta diseño

--- script.js
+++ script.js
@@ -15,22 +15,53 @@ let toDoList = [
 
 let ul = document.getElementById("toDoList");
 const mostrarList = () => {
-    ul.innerHTML = toDoList.map(toDo => `<li><label><input id="check" type="checkbox"> ${toDo.tarea}</label></li>`).join("");
+    ul.innerHTML = "";
+    toDoList.forEach(element => {
+        let li = document.createElement("li");
+        if (element.realizado) {
+            li.innerHTML = `<label id="label"><input id="check" value="${element.tarea}" type="checkbox" checked>${element.tarea}<button id="borrar" type="button" class="btn-close btn-sm" aria-label="Close"></button></label>`;
+            li.firstChild.style.textDecoration = "line-through";
+        } else {
+            li.innerHTML = `<label id="label"><input id="check" value="${element.tarea}" type="checkbox">${element.tarea}<button id="borrar" type="button" class="btn-close btn-sm" aria-label="Close"></button></label>`;
+            li.firstChild.style.textDecoration = "none";
+        }
+        ul.appendChild(li);
+    });
+    let inputs = document.querySelectorAll(`input[type="checkbox"]`);
+    inputs.forEach(element => {
+        element.onclick = (e) => {
+            if (e.target.checked) {
+                let index = busquedaLineal(e.target.value);
+                toDoList[index].realizado = true;
+                toDoList[index].fechaRealizado = new Date;
+                e.target.parentElement.style.textDecoration = "line-through";
+            }
+            else {
+                let index = busquedaLineal(e.target.value);
+                toDoList[index].realizado = false;
+                toDoList[index].fechaRealizado = null;
+                e.target.parentElement.style.textDecoration = "none";
+            }
+        };
+    });
+    let borrar = document.querySelectorAll("#borrar");
+    borrar.forEach(element => {
+        element.onclick = (e) => {
+            let index = busquedaLineal(e.target.parentElement.innerText);
+            console.log(e.target.parentElement.innerText);
+            toDoList.splice(index, 1);
+            console.log(toDoList);
+            mostrarList();
+        };
+    });
 }
-mostrarList();
 
-let checkBox = document.getElementById("check");
-checkBox.onclick = (e) => {
-    if (checkBox.checked) {
-        e.target.style.textDecoration = "line-through";
-        console.log("tachado");
-    }
-}
+mostrarList();
 
 let input = document.getElementById("tarea");
 let button = document.getElementById("enviar");
 
-button.onclick = () => {    
+button.onclick = () => {
     if (input.value.length == 0) {
         input.style.border = "1px solid red";
         alert("Complete los datos");
@@ -40,16 +71,44 @@ button.onclick = () => {
         toDoList.push({
             tarea: input.value,
             realizado: false,
-            fechaCreacion: Date.now(),
-            fechaRealizado: new Date
+            fechaCreacion: new Date,
+            fechaRealizado: null
         });
-        /*let li = document.createElement("li");
-        li.innerHTML = `<label><input class="check" type="checkbox"> ${input.value}</label>`;
-        ul.appendChild(li);*/
         mostrarList();
         input.style.border = "1px solid black";
         input.value = "";
     }
+
+    console.log(toDoList);
 };
 
-console.log(toDoList);
+let horaMax = document.getElementById("horaMax");
+let p = document.createElement("p");
+document.body.appendChild(p);
+horaMax.onclick = (e) => {
+    let tiempoEnRealizar = [];
+    toDoList.forEach(element => {
+        if (element.realizado) {
+            tiempoEnRealizar.push({
+                tiempo: element.fechaRealizado - element.fechaCreacion,
+                tarea: element.tarea
+            });
+        }
+    });
+    let arrayOrdenado = tiempoEnRealizar.sort((e1, e2) => e1.tiempo - e2.tiempo);
+    p.innerHTML = `La tarea que se realizo mas rapido fue: ${arrayOrdenado[0].tarea}`;
+    p.setAttribute("class", "display-4");
+}
+
+const busquedaLineal = tarea => {
+    let i = 0;
+    let pos = -1;
+    while (i < toDoList.length && pos === -1) {
+        if (toDoList[i].tarea === tarea) {
+            pos = i;
+        } else {
+            i++;
+        }
+    }
+    return pos;
+}
